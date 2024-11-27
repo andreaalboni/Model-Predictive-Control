57cc22dded2973d2edd44348f8936aeb7ee0b37e
andreaalboni <291587@studenti.unimore.it>
2024-11-27 22:21:54 +0100

    refactor: clean up code by removing unused functions and improving formatting in Session4.py

--- Assignment/assignment4.py
+++ Assignment/assignment4.py
@@ -1,11 +1,16 @@
-import math
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.patches import Circle, Rectangle
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 27 15:35:32 2024
+
+@author: carmi
+"""
+
 from typing import Callable, Tuple
 import sys 
 import casadi as cs 
 import os
+import string
+import math
 WORKING_DIR = os.path.split(__file__)[0]
 sys.path.append(os.path.join(WORKING_DIR, os.pardir))
 from given.parameters import VehicleParameters
@@ -14,7 +19,9 @@ from given.plotting import *
 
 from rcracers.simulator.dynamics import KinematicBicycle
 from rcracers.simulator import simulate
-
+import numpy as np
+import matplotlib.pyplot as plt 
+from matplotlib.patches import Rectangle, Circle
 
 def forward_euler(f, ts) -> Callable:
     def fw_eul(x,u):
@@ -23,11 +30,16 @@ def forward_euler(f, ts) -> Callable:
 
 def runge_kutta4(f, ts) -> Callable:
     def rk4_dyn(x,u):
-        s1 = f(x, u)
-        s2 = f(x + ts/2 * s1, u)
-        s3 = f(x + ts/2 * s2, u)
-        s4 = f(x + ts * s3, u)
-        return x + ts/6 * (s1 + 2*s2 + 2*s3 + s4)
+        #Begin TODO----------------------------------------------------------
+        #raise NotImplementedError("Implement the RK4 integrator!")
+        s1=f(x,u)
+        s2=f(x+ts/2*s1,u)
+        s3=f(x+ts/2*s2,u)
+        s4=f(x+ts*s3,u)
+        x_next = x+ts/6*(s1+2*s2+2*s3+s4)
+        #End TODO -----------------------------------------------------------
+        return x_next 
+    
     return rk4_dyn
 
 def exact_integration(f, ts) -> Callable:
@@ -57,6 +69,10 @@ def build_test_policy():
     policy = lambda y, t: np.array([acceleration, 0.1 * np.sin(t)])
     return policy
 
+#-----------------------------------------------------------
+# MPC CONTROLLER
+#-----------------------------------------------------------
+
 
 class MPCController:
 
@@ -79,6 +95,10 @@ class MPCController:
         
     def build_ocp(self, params: VehicleParameters) -> Tuple[dict, dict]:
         """
+        TODO IMPLEMENT
+
+        Build a nonlinear program that represents the parametric optimization problem described above, with the initial state x as a parameter. Use a single shooting formulation, i.e., do not define a new decision variable for the states, but rather write them as functions of the initial state and the control variables. Also return the lower bound and upper bound on the decision variables and constraint functions:
+
         Args: 
             VehicleParameters [params]: vehicle parameters
         Returns: 
@@ -98,76 +118,70 @@ class MPCController:
         """
         # Create a parameter for the initial state. 
         x0 = cs.SX.sym("x0", (4,1))
-
-        x = x0
-        u = [cs.SX.sym(f"u_{t}", (2,1)) for t in range(self.N)]
-        self.u = u
         
-        # Add upper− and lower bounds to x and u
-        lb_states = np.array([params.min_pos_x, params.min_pos_y, params.min_heading, params.min_vel])
-        ub_states= np.array([params.max_pos_x, params.max_pos_y, params.max_heading, params.max_vel])
+        #Begin TODO----------------------------------------------------------
+        #raise NotImplementedError("Implement the build step of your optimal control problem!")
+        N=self.N
+        x = x0
+        u=[cs.SX.sym(f"u_{t}", (2,1)) for t in range(N)]
+        self.u=u
+        # Add upper− and lower bounds to x and u.
+        
+        lb_states = np.array([params.min_pos_x,params.min_pos_y,params.min_heading,params.min_vel])
+        ub_states= np.array([params.max_pos_x,params.max_pos_y,params.max_heading,params.max_vel])
         
         lbu = np.array([params.min_drive,-params.max_steer])
         ubu = np.array([params.max_drive,params.max_steer])
         
-        Q = cs.diagcat(1, 3, 0.1, 0.01)
-        R = cs.diagcat(1, 0.01)
-        Q_N = 5 * Q
+        # Define the cost
         
-        cost = 0
-        lbx = []
-        ubx = []
-        g = []
-        lbg = []
-        ubg = []
-
-        ode = KinematicBicycle(params, symbolic=True)
-        f = runge_kutta4(ode, self.ts)
-
-        # Compute circles belonging to the stationary car
-        parkedcar_position = (0.25, 0.)
-        parkedcar_yaw = 0.
-        centers_parkedcar, radius_parkedcar = compute_circles(3, params.length, params.width, parkedcar_position, parkedcar_yaw)
-
-        for k in range(self.N):
-            cost += x.T @ Q @ x + u[k].T @ R @ u[k]
-            x = f(x,u[k])
-            lbx.append(lbu)
-            ubx.append(ubu)
+        Q=cs.diagcat(1,3,0.1,0.01)
+        R=cs.diagcat(1,0.01)
+        Q_N=5*Q
+        cost=0
+        lbx=[]
+        ubx=[]
+        g=[]
+        lbg=[]
+        ubg=[]
+        
+        ts=self.ts
+        ode=KinematicBicycle(params,symbolic=True)
+        f=runge_kutta4(ode, ts)
+        
+        centers_obstacle,r_obstacle=compute_circles(params.width,params.length,3,(0.25,0),0)
+        for k in range(N):
+          cost = cost + x.T @ Q @ x + u[k].T @ R @ u[k]
+          x=f(x,u[k])
+          lbx.append(lbu)
+          ubx.append(ubu)
+          g.append(x)
+          lbg.append(lb_states)
+          ubg.append(ub_states)
+          centers_controlled,r_controlled=compute_circles(params.width,params.length,3,(x[0],x[1]),x[2])
+          for center_controlled in centers_controlled:
+              for center_obstacle in centers_obstacle:
+                 distance=(center_controlled[0]-center_obstacle[0])**2+(center_controlled[1]-center_obstacle[1])**2
+                 squared_rad=(r_controlled+r_obstacle)**2
+                
+                 g.append(squared_rad-distance)
+                 lbg.append(-cs.inf)
+                 ubg.append(0)
+        
             
-            # Added state to g and the state's bounds to lbg and ubg
-            g.append(x)
-            lbg.append(lb_states)
-            ubg.append(ub_states)
-
-            centers_movingcar, radius_movingcar = compute_circles(3, params.length, params.width, (x[0], x[1]), x[2])
-            # Compute the distance between the center of the circles and the ones belonging to the stationary car
-            for cmc in centers_movingcar:
-                for cpc in centers_parkedcar:
-                    # Squared distance between the centers
-                    sq_dist = (cmc[0] - cpc[0])**2 + (cmc[1] - cpc[1])**2
-                    
-                    # Sqaured distance between the radii
-                    sq_rad = (radius_movingcar + radius_parkedcar)**2
-
-                    # Add the constraint to the list of constraints and the relative bounds
-                    g.append(sq_rad - sq_dist)
-                    lbg.append(-cs.inf)
-                    ubg.append(0)
-          
-        cost += x.T @ Q_N @ x
-        variables = cs.vertcat(*u)
+        cost+=x.T @ Q_N @ x
+        variables=cs.vertcat(*u)
         
         # Create the solver
-        nlp = {"f": cost, "x": variables, "g" : cs.vertcat(*g), "p" :x0}
+        nlp = {"f": cost,"x": variables,"g" : cs.vertcat(*g),"p" :x0}
         
-        bounds = {
+        bounds ={
             "lbx": cs.vertcat(*lbx),
             "ubx": cs.vertcat(*ubx),
             "lbg": cs.vertcat(*lbg),
             "ubg": cs.vertcat(*ubg)
             }
-        
+        #End TODO -----------------------------------------------------------
         return nlp, bounds
 
     def reshape_input(self, sol):
@@ -183,6 +197,11 @@ class MPCController:
         u = self.reshape_input(solution)
         return u[0]
 
+
+#-----------------------------------------------------------
+# UTILITIES
+#-----------------------------------------------------------
+
 def build_test_policy():
     # Define a policy to test the system
     acceleration = 1 # Fix a constant longitudinal acceleration 
@@ -196,83 +215,43 @@ def rel_error(val, ref):
         val - ref, axis=1, ord=np.inf,
     )/0.5*(1e-12 + np.linalg.norm(val, axis=1, ord=np.inf) + np.linalg.norm(ref, axis=1, ord=np.inf))
 
-
-
-def compute_circles(circles_number, length, width, car_position, car_yaw):
-    centers = []
-
-    # Definition of the car's rotation matrix
-    R = np.array([[math.cos(car_yaw), -math.sin(car_yaw)], [math.sin(car_yaw), math.cos(car_yaw)]])
+def compute_circles(w,l,n_c,car_position=(0,0),heading_angle=0):
+    rotation_matrix=np.array([[np.cos(heading_angle),-np.sin(heading_angle)],[np.sin(heading_angle),np.cos(heading_angle)]])
+    r=math.sqrt((l**2/(4*n_c**2))+(w/2)**2)
+    c=[]
+    for i in range(n_c):
+        x_c=-l/2+(l/(2*n_c)+i*2*l/(2*n_c))
+        c.append(rotation_matrix@np.array([x_c,0])+np.array(car_position))
+    return c,r
     
-    d = length / (2 * circles_number)
-    radius = math.sqrt(d**2 + (width/2)**2)
-
-    for i in range(circles_number):
-        xc = - length/2 + (d + i*2*d)
-        center = R @ np.array([xc, 0])
-        centers.append((center[0]+car_position[0], center[1]+car_position[1]))
-
-    return centers, radius
-
-def plot(nc, length, width, car_position=(0.0, 0.0), car_yaw=0.0):
-    R = Rectangle(
-            (-length/2+car_position[0], -width/2+car_position[1]), 
-            length, 
-            width, 
-            angle=car_yaw*180/math.pi,
-            rotation_point=car_position,
-            facecolor='none', 
-            edgecolor='black', 
-            linewidth=1
-        )
-    centers, radius = compute_circles(nc, length, width, car_position, car_yaw)
-
-    # Create a figure and axes
-    _, ax = plt.subplots()
-
-    for center in centers:
-        circle = Circle(center, radius, facecolor='none', edgecolor='cyan', linewidth=1)
+def assignment4_1(w,l,n_c,car_position=(0,0),heading_angle=0):
+    c,r=compute_circles(w,l,n_c,car_position,heading_angle)
+    fig, ax=plt.subplots()
+    rectangle=Rectangle([-l/2+car_position[0],-w/2+car_position[1]],l,w,angle=heading_angle*180/np.pi,rotation_point=car_position,facecolor='none',edgecolor='red')
+    for center in c:
+        circle=Circle(center,r,facecolor='none',edgecolor='blue')
         ax.add_patch(circle)
-
-    # Add the rectangle to the axes
-    ax.add_patch(R)
-
-    # Set the plot limits
-    lim = np.max([length, width])
-    ax.set_xlim(-lim + car_position[0], lim + car_position[0])
-    ax.set_ylim(-lim + car_position[1], lim + car_position[1])
-
-    # Set aspect ratio to be equal
+    ax.add_patch(rectangle)
+    limit=np.max([l,w])
+    ax.set_xlim(-limit+car_position[0],limit+car_position[0])
+    ax.set_ylim(-limit+car_position[1],limit+car_position[1])
     ax.set_aspect('equal')
-
-    # Display the plot
     plt.show()
-
-
-
-def Assignment41():
-    num_circles = 3
-    length = 4
-    width = 2
-    plot(num_circles, length, width)
-
-def Assignment42():
-    num_circles = 3
-    length = 4
-    width = 2
-    car_position = (2.0, 2.0)
-    car_yaw = math.pi / 4
-    plot(num_circles, length, width, car_position, car_yaw)
-
-def Assignment44():
-    N = 30
+    return c,r
+def assignment4_2():
+    p=(2,2)
+    psi=np.pi/4
+    assignment4_1(w=2,l=4,n_c=3,car_position=p,heading_angle=psi)
+    
+def assignment4_4():
+    
+    N=30
     ts = 0.08
     x0 = np.array([0.3, -0.1, 0, 0])
-    params = VehicleParameters()
-
+    params=VehicleParameters()
     print("--Set up the MPC controller")
     controller = MPCController(N=N, ts=ts, params=params)
-
+    
     print(f"--Solve the OCP for x0 = {x0}")
     solution = controller.solve(x0)
     controls = controller.reshape_input(solution)
@@ -283,7 +262,7 @@ def Assignment44():
     # Build the assumed model 
     bicycle = KinematicBicycle(params)
     dynamics_assumed = runge_kutta4(bicycle, ts)
-    #dynamics_assumed = forward_euler(bicycle, ts)
+    
 
     print(f"--Simulate under the assumed model")
     x_open_loop_model = simulate(x0, dynamics_assumed, n_steps=N, policy=open_loop_policy)
@@ -292,7 +271,7 @@ def Assignment44():
     print(f"--Simulate using more precise integration")
     dynamics_accurate = exact_integration(bicycle, ts)
     x_open_loop_exact = simulate(x0, dynamics_accurate, n_steps=N, policy=open_loop_policy)
-
+    
     print(f"--Plotting the results")
 
     print(f"---Plot Controls")
@@ -312,12 +291,13 @@ def Assignment44():
     plt.ylabel("$\| x - x_{pred} \| / \| x \| \\times 100$")
     plt.title("Relative prediction error (integration error) [%]")
     plt.show()
-
+    
+    
+    
 def main():
-    #Assignment41()
-    Assignment42()    
-
-    Assignment44()
-
+    assignment4_1(w=2,l=4,n_c=3)
+    assignment4_2()
+    assignment4_4()
+    
 if __name__ == "__main__":
     main()
